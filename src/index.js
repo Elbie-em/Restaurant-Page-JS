fbe1947f3610792dc344fe2cad4a5fd2f28ef462
elbie-em <emoonga1993@gmail.com>
2020-07-15 23:13:17 +0200

    add home container for headline and mission statement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+const container = document.getElementById('content');
 
 const navELement = () => {
-  let container = document.getElementById('content');
-
+  
   const nav = document.createElement('nav');
   nav.id = 'nav-bar';
   nav.classList.add('navbar', 'navbar-light', 'bg-dark','d-flex','flex-row','justify-content-between');
@@ -36,12 +36,12 @@ const navELement = () => {
 
   navRight.appendChild(ourServicesLink);
 
-  const specialsLink = document.createElement('a');
-  specialsLink.id = 'specials';
-  specialsLink.className = linkClass;
-  specialsLink.innerHTML = 'SPECIALS';
+  const menuLink = document.createElement('a');
+  menuLink.id = 'menu';
+  menuLink.className = linkClass;
+  menuLink.innerHTML = 'MENU';
 
-  navRight.appendChild(specialsLink);
+  navRight.appendChild(menuLink);
 
   const reserveLink = document.createElement('a');
   reserveLink.id = 'reservations';
@@ -54,9 +54,45 @@ const navELement = () => {
 
 }
 
+const homeContainer = () => {
+  const introContainer = document.createElement('div');
+  introContainer.id = 'intro';
+  introContainer.className = 'container mt-3 p-5 text-center';
+  introContainer.style.height = '500px';
+
+  const logo = document.createElement('img');
+  logo.src = './dist/assets/images/logo.png';
+  logo.className = 'heading-logo'
+  introContainer.appendChild(logo);
+
+  const heading = document.createElement('h1');
+  heading.className = 'custom-font-a text-white big-font'
+  heading.innerHTML = "Welcome to El's Kitchen"
+
+  introContainer.appendChild(heading);
+
+  const missionStat = document.createElement('p');
+  missionStat.className = 'custom-font-b text-white medium-font';
+  missionStat.innerHTML = `We imagine a world where there’s no such thing as a bad meal from our highly trained chefs,all our customers leave happily.<br> We Serve Londons best cuisine!`;
+
+  introContainer.appendChild(missionStat);
+
+  const reserveButton = document.createElement('button');
+  reserveButton.id = 'reserve-btn';
+  reserveButton.className = "btn btn-secondary text-white custom-font-b"
+  reserveButton.innerHTML = 'Make Your Reservation';
+
+  introContainer.appendChild(reserveButton);
+
+  container.appendChild(introContainer);
+
+};
+
+
 
 const render = () => {
   navELement();
+  homeContainer();
 }
 
 
